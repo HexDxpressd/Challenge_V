e4fd686edaa0a8fc134d43a440b6567d763ebe59
Juan Esteban Conde G <condejuan044@gmail.com>
2022-12-09 22:48:24 -0500

    .

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const mysql = require("mysql")
 const app = express()
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
-
 const conectBD = mysql.createConnection({
     host: "localhost",
     user: "root",
